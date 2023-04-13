9ec0b7e6d7ce0a8d6d1d471bf77622c1a8d496ac
Beknasar <680633@gmail.com>
2023-04-13 13:51:06 +0300

    Created get a Random Cafe route.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 app = Flask(__name__)
 
-##Connect to Database
+# #Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.app_context().push()
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-##Cafe TABLE Configuration
+# # Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -23,19 +25,42 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        # Method 1.
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+        # Method 2. Alternatively use Dictionary Comprehension to do the same thing.
+        # return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+db.create_all()
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
     
 
-## HTTP GET - Read Record
+# # HTTP GET - Read Record
+@app.route('/random')
+def get_random_cafe():
+    all_cafes = db.session.query(Cafe).all()
+    random_cafe = random.choice(all_cafes)
+    # Simply convert the random_cafe data record to a dictionary of key-value pairs.
+    return jsonify(cafe=random_cafe.to_dict())
 
-## HTTP POST - Create Record
+# # HTTP POST - Create Record
 
-## HTTP PUT/PATCH - Update Record
+# # HTTP PUT/PATCH - Update Record
 
-## HTTP DELETE - Delete Record
+# # HTTP DELETE - Delete Record
 
 
 if __name__ == '__main__':
